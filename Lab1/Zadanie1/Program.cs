0cc4500822f658dc0892b1ab5bde4bb721a55321
RGusar <rgusar@mail.ru>
2017-10-17 20:32:11 +0600

    Решено задания 1-2 в Лабораторной 1

---
+++ Lab1/Zadanie1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ура...\"Заработало!\"");
+            Console.WriteLine("+---+\n"+
+            "|   |\n"+
+            "+---+\n");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("#");
+            Console.WriteLine("##");
+            Console.WriteLine("###");
+            Console.WriteLine("####");
+            Console.WriteLine("\n");
+            Console.WriteLine("А теперь Елочка");
+            Console.WriteLine("  /\\");
+            Console.WriteLine(" //\\\\");
+            Console.WriteLine("///\\\\\\");
+            Console.WriteLine("  ][    ");
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("{ \"x\":15, \"y\": 28.7 }");
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("Три девицы под окном \n"+
+                "Пряли поздно вечерком. \n"+
+                "\"Кабы я была царица, — \n"+
+                "Говорит одна девица, — \n" +
+                "То на весь крещеный мир \n"+
+                "Приготовила б я пир\" \n"+
+                "\"Кабы я была царица, — \n"+
+                "Говорит ее сестрица, - \n"+
+                "То на весь бы мир одна \n"+
+                "Наткала я полотна\". \n"+
+                "\"Кабы я была царица, — \n"+
+                "Третья молвила сестрица, — \n"+
+                "Я б для батюшки - царя \n"+
+                "Родила богатыря\".");
+
+
+
+        }
+    }
+}
---
+++ Lab1/Zadanie3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a;
+            a=Console.Write("Программирование это");
+        }
+
+    }
+}
