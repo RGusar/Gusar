665881d3fc66c5ceb6fc3de7ffc484c100d1ad8d
RGusar <rgusar@mail.ru>
2018-01-09 20:26:16 +0600

    Лаб 8 5ую чуть доделать

---
+++ Lab8/z4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int B;
+            B = Convert.ToInt32(Console.ReadLine());
+            
+            int E;
+            E = Convert.ToInt32(Console.ReadLine());
+
+           
+            String data;
+            data = Console.ReadLine();
+            String[] arr = data.Split(' ');
+
+            if (B < 0)
+            {
+                Console.WriteLine("Число B должно быть в интервале [0" + "-" + arr.Length +"]");
+                return;
+            }
+
+            if (B > arr.Length)
+            {
+                Console.WriteLine("Число B должно быть в интервале [0" + "-" + arr.Length + "]");
+                return;
+            }
+            
+            if (E > arr.Length)
+            {
+                Console.WriteLine("Число B должно быть в интервале [0" + "-" + arr.Length + "]");
+                return;
+            }
+
+            int i;
+            i = 0;
+           
+            if (B > E)
+            {
+                i = E;
+                while (i < B)
+                {
+                    Console.WriteLine(arr[i]);
+                    i++;
+                }
+            }
+            else
+            {
+                i = B;
+                while (i <= E)
+                {
+                    Console.WriteLine(arr[i]);
+                    i++;
+                }
+                    
+            }
+                    
+
+            
+
+            
+
+
+
+
+        }
+    }
+}
---
+++ Lab8/z5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int P;
+            P = Convert.ToInt32(Console.ReadLine());
+
+            int Q;
+            Q= Convert.ToInt32(Console.ReadLine());
+
+            String data;
+            data = Console.ReadLine();
+            String[] arr = data.Split(' ');
+
+            String T;
+
+            T = arr[P];
+            arr[P] = arr[Q];
+            arr[Q] = T;
+
+            int i;
+            i = 0;
+
+            while (i < arr.Length)
+            {
+                Console.Write(arr[i]);
+                i++;
+            }
+        }
+    }
+}
