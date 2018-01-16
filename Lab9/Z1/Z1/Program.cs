4cf7d16b63105340aa278227cddefa61ad503144
RGusar <rgusar@mail.ru>
2018-01-16 20:15:55 +0600

    Лаб 9 З.1-2

---
+++ Lab9/Z1/Z1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename;
+            filename = "../../task6175/test1.txt";
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("Файл не существует");
+                return;
+            }
+            StreamReader reader;
+            reader = new StreamReader(filename);
+            if (reader.EndOfStream)
+            {
+                Console.WriteLine("Файл пуст");
+            }
+            else
+            {
+                String line = reader.ReadLine();
+                Console.Write(line);
+            }
+           
+              while (!reader.EndOfStream)
+
+            {
+                String line = reader.ReadLine();
+                Console.Write(", " + line);
+            }
+
+            
+        }
+    }
+}
+        
---
+++ Lab9/Z2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename;
+            filename = "../../task6175/test2.txt";
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("Файл не существует");
+                return;
+            }
+            StreamReader reader;
+            reader = new StreamReader(filename);
+            if (reader.EndOfStream)
+            {
+                Console.WriteLine("Файл пуст");
+            }
+            else
+            {
+                String line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+
+            while (!reader.EndOfStream)
+
+            {
+                String line = reader.ReadLine();
+                Console.WriteLine(", " + line);
+            }
+        }
+    }
+}
